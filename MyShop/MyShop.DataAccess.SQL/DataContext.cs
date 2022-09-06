e02819995ada4eea1244b55d53d56c32b58f4f53
kerxiong10 <82770075+kerxiong10@users.noreply.github.com>
2022-09-05 20:11:50 -0500

    Added Entity Framework

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() 
+            : base("DefaultConnection") //will cause underline DB context to lok in our webconfig and look for a section call "DefaultConnection"
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
